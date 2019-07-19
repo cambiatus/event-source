36714c5e3b1f9ea47ae4a4c45a297551bcc340c9
Zacck Osiemo <coderv63@gmail.com>
2019-07-19 16:11:26 +0200

    Do error handling when finding an action

--- src/updaters/community.js
+++ src/updaters/community.js
@@ -356,25 +356,31 @@ function verifyAction (db, payload, blockInfo, context) {
   console.log(`BeSpiral  >>> Action verification`)
 
   // Collect the action
-  const savedAction = db.actions.findOne(payload.data.action_id).then(a => a)
-
-  // Compute Completion
-  let completed = false
-  if (savedAction.usages_left - 1 <= 0) {
-    completed = true
-  }
-
-  const updateData = {
-    usages_left: savedAction.usages_left - 1,
-    is_completed: completed
-  }
-
   db.actions
-    .update({
-      id: payload.data.action_id
-    }, updateData)
+    .findOne(payload.data.action_id)
+    .then(a => {
+      let completed = false
+
+      if (a.usages_left - 1 <= 0) {
+        completed = true
+      }
+
+      const updateData = {
+        usages_left: a.usages_left - 1,
+        is_completed: completed
+      }
+
+      db.actions
+        .update({
+          id: payload.data.action_id
+        }, updateData)
+        .catch(e => {
+          console.log('Something went wrong while verifying an action', e)
+          Sentry.captureException(e)
+        })
+    })
     .catch(e => {
-      console.log('Something went wrong while verifying an action', e)
+      console.log('Something went wrong while finding an action', e)
       Sentry.captureException(e)
     })
 }
