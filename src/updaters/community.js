c2f8485a0614020b83a897906965de434c8b0afa
Alisson Morais <alissonfpmorais@gmail.com>
2019-08-04 17:59:02 -0300

    fix payload data to look for 'verifier' instead of 'validator'

--- src/updaters/community.js
+++ src/updaters/community.js
@@ -411,7 +411,7 @@ function verifyClaim (db, payload, blockInfo, context) {
 
   const checkData = {
     claim_id: payload.data.claim_id,
-    validator_id: payload.data.validator,
+    validator_id: payload.data.verifier,
     is_verified: payload.data.vote === 1,
     created_block: blockInfo.blockNumber,
     created_tx: payload.transactionId,
