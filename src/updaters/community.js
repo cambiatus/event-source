d51420fc62cc868438e59033a61e370f5fee075c
Lucca <julienlucca@gmail.com>
2020-07-07 13:21:16 -0300

    fix criterea to make an action as completed

--- src/updaters/community.js
+++ src/updaters/community.js
@@ -568,7 +568,7 @@ function verifyClaim (db, payload, blockInfo, context) {
                     if (!action.is_completed && action.usages > 0) {
                       tx.actions.update(action.id, {
                         usages_left: action.usages_left - 1,
-                        is_completed: action.usages_left - 1 === 0
+                        is_completed: action.usages_left - 1 !== 0
                       })
                     }
                   }
