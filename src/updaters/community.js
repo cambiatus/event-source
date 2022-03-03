2088307c6f9e180712ccab48fc938934c2d42437
Julien Lucca <julienlucca@gmail.com>
2022-03-03 19:17:01 -0300

    Add async

--- src/updaters/community.js
+++ src/updaters/community.js
@@ -700,7 +700,7 @@ async function upsertRole(db, payload, blockInfo, _context) {
 async function assignRole(db, payload, blockInfo, _context) {
   console.log('Cambiatus >>> Assign Role', blockInfo.blockNumber)
 
-  payload.data.roles.map(roleName => {
+  payload.data.roles.map(async roleName => {
     const foundNetwork = await db.network.findOne({ community_id: payload.data.commmunity_id, account_id: payload.data.member })
     if (foundNetwork == null) throw new Error('Network not found. Might have a database sync error')
 
